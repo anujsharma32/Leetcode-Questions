22f703390e0471a961a39446e9c227ae1c795311
anujsharma32 <anujdps123@gmail.com>
2026-07-04 14:40:01 +0530

    done

---
+++ Array/maximum.java
@@ -0,0 +1,12 @@
+public class maximum {
+    public static void main(String[] args) {
+        int arr[]={3,6,1,4};
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+        }
+}
