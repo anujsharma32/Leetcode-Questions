f77472a743df72247f762b23fe152ef0163054ad
anujsharma32 <anujdps123@gmail.com>
2026-07-05 14:14:46 +0530

    solution for segregate an array of 0s and 1s

---
+++ Array/segregate.java
@@ -0,0 +1,22 @@
+public class segregate {
+    public static void main(String[] args) {
+        int arr[]={1,0,1,1,1,1};
+        int zeros=0;
+        int ones=0;
+        for(int el : arr){
+            if(el==0)zeros++;
+            else ones++;
+        }
+        for(int i=0;i<zeros;i++){
+            arr[i]=0;
+        }
+        for(int j=zeros;j<arr.length;j++){
+            arr[j]=1;
+        }
+        System.out.println("Array after segregation:");
+       for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+       }
+    }
+    
+}
