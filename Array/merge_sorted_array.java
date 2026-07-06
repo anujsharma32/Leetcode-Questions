dffc3306863f517360826bcb9fb70dad5208b576
anujsharma32 <anujdps123@gmail.com>
2026-07-06 19:48:23 +0530

    Updated merge sorted array solution

--- Array/merge_sorted_array.java
+++ Array/merge_sorted_array.java
@@ -1,31 +1,30 @@
 public class merge_sorted_array {
     public static void main(String[] args) {
-        int[] a ={1,3,6,8,10,20};
-        int[] b ={2,6,8,10,12,40};
-        int[] c = new int[a.length+b.length];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<a.length && j<b.length){
-            if(a[i]<b[j]){
-                c[k] = a[i];
-                i++;
-            }
-            else{
-                c[k]=b[j];
-                j++;
-            }
-            k++;
-            if(i==a.length && j<b.length){
-                c[k++]=b[j++];
-            }
-            if(j==b.length && i<a.length){
-                c[k++]=a[i++];
+        int[] a = {1, 3, 6, 8, 10, 20};
+        int[] b = {2, 6, 8, 10, 12, 40};
+
+        int[] c = new int[a.length + b.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            } else {
+                c[k++] = b[j++];
             }
         }
 
-        for(int el=0;el<c.length;el++){
-            System.out.print(c[el]+" ");
+        while (i < a.length) {
+            c[k++] = a[i++];
+        }
+
+        while (j < b.length) {
+            c[k++] = b[j++];
+        }
+
+        for (int el : c) {
+            System.out.print(el + " ");
         }
     }
 }
